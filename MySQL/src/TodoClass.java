0f43e0527e381108f5c4e8e935ae2cd5eb7c7021
megoRU <savin.yura2014@gmail.com>
2020-03-16 23:34:50 +0300

    Revert "v2.0"
    This reverts commit 61caf5dd6da72c717f9a780d6c47eb850336431e.

--- MySQL/src/TodoClass.java
+++ MySQL/src/TodoClass.java
@@ -5,23 +5,16 @@ import java.util.TreeMap;
 public class TodoClass {
 
   public final String COMMAND_ADD = "ADD\\s+.+";
-  public final String COMMAND_ADD_RU = "адд\\s+.+";
   public final String COMMAND_ADD_TO_INDEX = "ADD\\s+\\d+\\s+.+";
-  public final String COMMAND_ADD_TO_INDEX_RU = "адд\\s+\\d+\\s+.+";
   public final String COMMAND_EDIT = "EDIT\\s+\\d+\\s+.+";
-  public final String COMMAND_EDIT_RU = "изменить\\s+\\d+\\s+.+";
   public final String COMMAND_DELETE = "DELETE\\s+\\d+";
-  public final String COMMAND_DELETE_RU = "удалить\\s+\\d+";
-  public final String COMMAND_DELETE_ALL = "DELETE ALL";
-  public final String COMMAND_DELETE_ALL_RU = "удалить все";
+  public final String COMMAND_DELETE_ALL = "DELETEALL";
   public final String COMMAND_LIST = "LIST";
-  public final String COMMAND_LIST_RU = "лист";
-  public final String COMMAND_DELETE_BD = "удалить бд";
 
   private TreeMap<String, String> todoList = new TreeMap<>();
 
   /**
-   * Заменить данные на свои; CONN = "jdbc:mysql://ip_adress_or_domain:3306/BD_name";
+   * db3.myarena.ru Заменить данные на свои; CONN = "jdbc:mysql://ip_adress_or_domain:3306/BD_name";
    * <p>
    * USER = "BD_name";
    * <p>
@@ -30,10 +23,9 @@ public class TodoClass {
    * SQL База данных которую можно просто импортировать:
    **/
 
-  private static final String CONN = "jdbc:mysql://db3.myarena.ru:3306/u2864_skillbox2";
-  private static final String USER = "u2864_skillbox2";
-  private static final String PASS = "9S7j1D0b";
-  private static final String SQL_DROP_TABLE = "DROP TABLE Todolist";
+  private static final String CONN = "";
+  private static final String USER = "";
+  private static final String PASS = "";
 
   private static final String SQL_CREATE = "CREATE TABLE IF NOT EXISTS Todolist"
       + "("
@@ -43,18 +35,7 @@ public class TodoClass {
       + " UNIQUE KEY text (text)"
       + ")";
 
-  public void dropTable(String command) {
-    try {
-      Connection conn = DriverManager.getConnection(CONN, USER, PASS);
-      PreparedStatement preparedStatement = conn.prepareStatement(SQL_DROP_TABLE);
-      preparedStatement.executeUpdate();
-      createTable(); //создаем
-    } catch (Exception e) {
-      System.err.println(e.getMessage());
-    }
-  }
-
-  private void createTable() {
+  private void createTable() throws Exception {
     try {
       Connection conn = DriverManager.getConnection(CONN, USER, PASS);
       PreparedStatement preparedStatement = conn.prepareStatement(SQL_CREATE);
@@ -64,7 +45,7 @@ public class TodoClass {
     }
   }
 
-  public void getCreateTable() {
+  public void getCreateTable() throws Exception {
     createTable();
   }
 
@@ -78,7 +59,7 @@ public class TodoClass {
       preparedStmt.setInt(1, num() + 1); // Получаем "длину" таблицы и прибавляем 1
       preparedStmt.setString(2, todoText);
       preparedStmt.execute(); //Записываем данные в БД
-      System.err.println("Заметка сохранена!");
+      System.err.println("Заметка сохранена");
       conn.close();
     } catch (Exception e) {
       System.err.println("Получена ошибка!");
@@ -89,7 +70,7 @@ public class TodoClass {
   /**
    * Подсчитываем колличество записей в таблице
    **/
-  public int num() {
+  public int num() throws Exception {
     try {
       Class.forName("com.mysql.jdbc.Driver");
       Connection conn = DriverManager.getConnection(CONN, USER, PASS);
@@ -98,23 +79,23 @@ public class TodoClass {
       while (resultSet.next()) {
         return resultSet.getInt(1);
       }
-    } catch (Exception ignored) {
+    } catch (Exception e) {
     }
     return 0;
   }
 
-  public void addToIndex(String command) {
+  public void addToIndex(String command) throws Exception {
     String[] commandArray = command.split("\\s+", 3);
     String todoTextAddToIndex = commandArray[2];
     String indexText = commandArray[1];
-
     int addToIndex = Integer.parseInt(indexText);
+
     if (addToIndex == 0) {
       System.out.println("Индекс не может быть нулём!");
     }
-    if (addToIndex <= num() || addToIndex >= num()) {
-      int addToIndex2 = num() + 1;
-      System.err.println("Индекс изменен на: " + addToIndex2);
+    if (addToIndex >= num())
+    {
+    int addToIndex2 = addToIndex + 1;
       try {
         Class.forName("com.mysql.jdbc.Driver");
         Connection conn = DriverManager.getConnection(CONN, USER, PASS);
@@ -134,7 +115,8 @@ public class TodoClass {
     }
   }
 
-  public void editByIndex(String command) {
+  public void edit(String command) {
+
     String[] commandArray = command.split("\\s+", 3);
     String todoTextEdit = commandArray[2];
     String indexText = commandArray[1];
@@ -156,9 +138,7 @@ public class TodoClass {
   }
 
   public void deleteAll(String command) {
-    if (num() == 0) {
-      System.err.println("Заметок нет! Зачем их удалять :)");
-    } else if (command.equals(COMMAND_DELETE_ALL) || command.equals(COMMAND_DELETE_ALL_RU)) {
+    if (command.equals(COMMAND_DELETE_ALL)) {
       try {
         Class.forName("com.mysql.jdbc.Driver");
         Connection conn = DriverManager.getConnection(CONN, USER, PASS);
@@ -174,7 +154,7 @@ public class TodoClass {
     }
   }
 
-  public void deleteByIndex(String command) {
+  public void delete(String command) {
     String todoTextDelete = command.replaceAll("[^0-9]", "").trim();
     int indexRemove = Integer.parseInt(todoTextDelete);
     try {
@@ -184,7 +164,7 @@ public class TodoClass {
       PreparedStatement preparedStmt = conn.prepareStatement(query3);
       preparedStmt.setInt(1, indexRemove);
       preparedStmt.executeUpdate();
-      System.err.println("Заметка удалена!\n>");
+      System.err.println("Заметка удалена!");
       conn.close();
     } catch (Exception e) {
       System.err.println("Получена ошибка!");
@@ -192,7 +172,7 @@ public class TodoClass {
     }
   }
 
-  public void list(String input) {
+  public void list(String command) throws Exception {
     if (num() == 0) {
       System.err.println("Заметок нет!");
     } else {
@@ -203,7 +183,7 @@ public class TodoClass {
         Statement statement = conn.createStatement();
         String sql = "SELECT id, text FROM Todolist ORDER BY id ASC";
         ResultSet rs = statement.executeQuery(sql);
-        System.err.println("Список всех заметок: ");
+        System.err.print("Список всех заметок: ");
         while (rs.next()) {
           String id = rs.getString("id");
           String text = rs.getString("text");
