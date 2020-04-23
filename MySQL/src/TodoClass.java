3a71fb933a10dd2f98b29672a36e519e78da96d7
megoRU <savin.yura2014@gmail.com>
2020-04-23 14:45:41 +0300

    Update TodoClass.java
    1. Code cleaning
    2. Removing warning
    3. Adding new commands

--- MySQL/src/TodoClass.java
+++ MySQL/src/TodoClass.java
@@ -1,24 +1,30 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.util.TreeMap;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TodoClass {
 
-  public final String COMMAND_ADD = "ADD\\s+.+";
+  public final String COMMAND_ADD = "add\\s+.+";
   public final String COMMAND_ADD_RU = "адд\\s+.+";
-  public final String COMMAND_ADD_TO_INDEX = "ADD\\s+\\d+\\s+.+";
+  public final String COMMAND_ADD_TO_INDEX = "add\\s+\\d+\\s+.+";
   public final String COMMAND_ADD_TO_INDEX_RU = "адд\\s+\\d+\\s+.+";
-  public final String COMMAND_EDIT = "EDIT\\s+\\d+\\s+.+";
+  public final String COMMAND_EDIT = "edit\\s+\\d+\\s+.+";
   public final String COMMAND_EDIT_RU = "изменить\\s+\\d+\\s+.+";
-  public final String COMMAND_DELETE = "DELETE\\s+\\d+";
+  public final String COMMAND_DELETE = "delete\\s+\\d+";
+  public final String COMMAND_DEL = "del\\s+\\d+";
   public final String COMMAND_DELETE_RU = "удалить\\s+\\d+";
-  public final String COMMAND_DELETE_ALL = "DELETE ALL";
+  public final String COMMAND_DELETE_ALL = "delete all";
   public final String COMMAND_DELETE_ALL_RU = "удалить все";
-  public final String COMMAND_LIST = "LIST";
+  public final String COMMAND_LIST = "list";
   public final String COMMAND_LIST_RU = "лист";
   public final String COMMAND_DELETE_BD = "удалить бд";
+  public final String ALL_LETTERS_AND_NUMBERS = "^[A-ZА-Я]+[А-Яа-яA-Za-z0-9\\s+]+";
 
-  private TreeMap<String, String> todoList = new TreeMap<>();
+  //private final TreeMap<String, String> todoList = new TreeMap<>();
 
   /**
    * Заменить данные на свои; CONN = "jdbc:mysql://ip_adress_or_domain:3306/BD_name";
@@ -39,11 +45,12 @@ public class TodoClass {
       + "("
       + " id int(6) NOT NULL AUTO_INCREMENT,"
       + " text varchar(128) NOT NULL,"
+      + " time varchar(128) NOT NULL,"
       + " PRIMARY KEY (id),"
       + " UNIQUE KEY text (text)"
       + ")";
 
-  public void dropTable(String command) {
+  private void dropTable() {
     try {
       Connection conn = DriverManager.getConnection(CONN, USER, PASS);
       PreparedStatement preparedStatement = conn.prepareStatement(SQL_DROP_TABLE);
@@ -64,9 +71,34 @@ public class TodoClass {
     }
   }
 
-  //Создаем таблицу
   public void getCreateTable() {
     createTable();
+  }
+
+  public void getDropTable() {
+    dropTable();
+  }
+
+  public void addText(String command) {
+    String todoText = command.trim();
+      try {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(CONN, USER, PASS);
+        Date dateNow = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy '|' HH:mm:ss");
+        String dateADD = formatForDateNow.format(dateNow);
+        String query = "INSERT INTO Todolist (id, text, time)" + " values (?, ?, ?)";
+        PreparedStatement preparedStmt = conn.prepareStatement(query);
+        preparedStmt.setInt(1, num() + 1); // Получаем "длину" таблицы и прибавляем 1
+        preparedStmt.setString(2, todoText);
+        preparedStmt.setString(3, dateADD);
+
+        preparedStmt.execute(); //Записываем данные в БД
+        System.err.println("Заметка сохранена!");
+        conn.close();
+      } catch (Exception e) {
+        System.err.println(e.getMessage());
+      }
   }
 
   public void add(String command) {
@@ -74,15 +106,19 @@ public class TodoClass {
     try {
       Class.forName("com.mysql.jdbc.Driver");
       Connection conn = DriverManager.getConnection(CONN, USER, PASS);
-      String query = "INSERT INTO Todolist (id, text)" + " values (?, ?)";
+      Date dateNow = new Date();
+      SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy '|' HH:mm:ss");
+      String dateADD = formatForDateNow.format(dateNow);
+      String query = "INSERT INTO Todolist (id, text, time)" + " values (?, ?, ?)";
       PreparedStatement preparedStmt = conn.prepareStatement(query);
       preparedStmt.setInt(1, num() + 1); // Получаем "длину" таблицы и прибавляем 1
       preparedStmt.setString(2, todoText);
+      preparedStmt.setString(3, dateADD);
+
       preparedStmt.execute(); //Записываем данные в БД
       System.err.println("Заметка сохранена!");
       conn.close();
     } catch (Exception e) {
-      System.err.println("Получена ошибка!");
       System.err.println(e.getMessage());
     }
   }
@@ -96,7 +132,7 @@ public class TodoClass {
       Connection conn = DriverManager.getConnection(CONN, USER, PASS);
       Statement statement = conn.createStatement();
       ResultSet resultSet = statement.executeQuery("SELECT COUNT(id) AS id FROM Todolist");
-      while (resultSet.next()) {
+      if (resultSet.next()) {
         return resultSet.getInt(1);
       }
     } catch (Exception ignored) {
@@ -119,17 +155,22 @@ public class TodoClass {
       try {
         Class.forName("com.mysql.jdbc.Driver");
         Connection conn = DriverManager.getConnection(CONN, USER, PASS);
+
         //Получаем данные
-        String query = "INSERT INTO Todolist (id, text)" + " values (?, ?)";
+        Date dateNow = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy '|' HH:mm:ss");
+        String dateADD = formatForDateNow.format(dateNow);
+        String query = "INSERT INTO Todolist (id, text, time)" + " values (?, ?, ?)";
         PreparedStatement preparedStmt = conn.prepareStatement(query);
         preparedStmt.setInt(1, addToIndex2);
         preparedStmt.setString(2, todoTextAddToIndex);
+        preparedStmt.setString(3, dateADD);
+
         // Записываем все данные в Базу Данных
         preparedStmt.execute();
         System.err.println("Заметка сохранена!");
         conn.close();
       } catch (Exception e) {
-        System.err.println("Получена ошибка!");
         System.err.println(e.getMessage());
       }
     }
@@ -151,7 +192,6 @@ public class TodoClass {
       System.err.println("Заметка изменена!");
       conn.close();
     } catch (Exception e) {
-      System.err.println("Получена ошибка!");
       System.err.println(e.getMessage());
     }
   }
@@ -169,7 +209,6 @@ public class TodoClass {
         System.err.println("Заметки удалены!");
         conn.close();
       } catch (Exception e) {
-        System.err.println("Получена ошибка!");
         System.err.println(e.getMessage());
       }
     }
@@ -178,22 +217,26 @@ public class TodoClass {
   public void deleteByIndex(String command) {
     String todoTextDelete = command.replaceAll("[^0-9]", "").trim();
     int indexRemove = Integer.parseInt(todoTextDelete);
+    int indexReSetId = Integer.parseInt(todoTextDelete);
     try {
       Class.forName("com.mysql.jdbc.Driver");
       Connection conn = DriverManager.getConnection(CONN, USER, PASS);
       String query3 = "DELETE FROM Todolist WHERE id = ?";
+      String query4 = "UPDATE Todolist SET id = id - 1 WHERE id >= ?";
       PreparedStatement preparedStmt = conn.prepareStatement(query3);
+      PreparedStatement preparedStmt2 = conn.prepareStatement(query4);
       preparedStmt.setInt(1, indexRemove);
+      preparedStmt2.setInt(1, indexReSetId);
       preparedStmt.executeUpdate();
+      preparedStmt2.executeUpdate();
       System.err.println("Заметка удалена!\n>");
       conn.close();
     } catch (Exception e) {
-      System.err.println("Получена ошибка!");
       System.err.println(e.getMessage());
     }
   }
 
-  public void list(String input) {
+  public void list() {
     if (num() == 0) {
       System.err.println("Заметок нет!");
     } else {
@@ -202,23 +245,46 @@ public class TodoClass {
         Connection conn = DriverManager.getConnection(CONN, USER, PASS);
         //Получаем данные и выводим
         Statement statement = conn.createStatement();
-        String sql = "SELECT id, text FROM Todolist ORDER BY id ASC";
+        String sql = "SELECT id, text, time FROM Todolist ORDER BY id ASC";
         ResultSet rs = statement.executeQuery(sql);
         System.err.println("Список всех заметок: ");
         while (rs.next()) {
           String id = rs.getString("id");
           String text = rs.getString("text");
+          String time = rs.getString("time");
           //Вывод данных
-          todoList.put(id, text);
-          System.out.print("\n" + id + ". " + text);
+         // todoList.put(id, text);
+          System.out.print("\n" + id + ". " + text + " | Дата создания: " + time);
         }
         System.out.println();
         rs.close();
         conn.close();
       } catch (Exception e) {
-        System.err.println("Получена ошибка!");
         System.err.println(e.getMessage());
       }
     }
+  }
+
+  public void availableCommands() {
+    System.out.print("Доступные команды:");
+    System.out.print(""
+        + "\nadd, "
+        + "add Index, "
+        + "edit Index, "
+        + "delete index, "
+        + "del index, "
+        + "delete all, "
+        + "list");
+    System.out.print(""
+        + "\nадд, "
+        + "адд индекс, "
+        + "изменить индекс, "
+        + "удалить индекс, "
+        + "удалить все, "
+        + "лист");
+    System.out.print(""
+        + "\nP. S. Index/индекс = цифра");
+    System.out.println(""
+        + "\nP. S.2 Если слово начинается с большой буквы, то комманду писать не нужно!");
   }
 }
